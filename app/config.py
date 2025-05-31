cd9253d64879d1d1ef3a642aa6cef4da0a3675a0
Teagan Glenn <that@teagantotally.rocks>
2025-05-31 01:50:07 -0600

    Use raw for re

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ from typing import Optional
 HA_URL: str = os.environ.get("HA_URL", "http://localhost:8123")
 HA_TOKEN: str = os.environ.get("HA_TOKEN", "")
 HA_FILTER_EXPOSED: bool = os.environ.get("HA_FILTER_EXPOSED", "false").lower() in ["1", "true", "yes"]
-HA_WS_URL: str = re.sub("\/?$", "/api/websocket", HA_URL.replace("http", "ws")
+HA_WS_URL: str = re.sub(r"\/?$", "/api/websocket", HA_URL.replace("http", "ws")
 
 def get_ha_headers() -> dict:
     """Return the headers needed for Home Assistant API requests"""
